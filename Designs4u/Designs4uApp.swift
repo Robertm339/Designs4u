29e8e0bde570b9c30b98926f0431d0ae7c5960b5
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2023-02-03 09:36:55 -0800

    Initial Commit

---
+++ Designs4u/Designs4uApp.swift
@@ -0,0 +1,17 @@
+//
+//  Designs4uApp.swift
+//  Designs4u
+//
+//  Created by Robert Martinez on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Designs4uApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
